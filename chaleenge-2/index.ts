7e0ea4812ac07e3b514c380584ee13b290e4eaa5
UJ528491 <hyj0829@gmail.com>
2021-08-07 23:18:05 +0900

    No BS TS Challenge #2

---
+++ chaleenge-2/index.ts
@@ -0,0 +1,24 @@
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
+  items.reduce((a, v) => {
+    forEachFunc(v);
+    return undefined;
+  }, undefined);
+}
+
+myForEach(["a", "b", "c"], v => console.log(`forEach ${v}`));
+
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+  return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [] as T[]);
+}
+
+console.log(myFilter([1, 2, 3, 4, 5, 6, 7, 8], v => v % 2 === 0));
+
+function myMap<T, K>(items: T[], mapFunc: (v: T) => K): K[] {
+  return items.reduce((a, v) => [...a, mapFunc(v)], [] as K[]);
+  // return items.reduce((a: T[], v) => (filterFunc(v) ? [...a, v] : a), []);
+  // return items.reduce<T[]>((a, v) => (filterFunc(v) ? [...a, v] : a), []);
+  // return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [] as T[]);
+  // return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), <T[]>[]);
+}
+
+console.log(myMap([1, 2, 3, 4, 5, 6, 7, 8], v => (v * 10).toString()));
